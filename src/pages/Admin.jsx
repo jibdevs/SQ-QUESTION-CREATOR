948b94900d2bd2223aa0674ed5cb5f9c1a7c0b45
Jib <jib.mak91@gmail.com>
2026-07-26 19:33:12 +0200

    update admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,126 +1,23 @@
-import "./Admin.css";
-
-
-function Admin(){
-
-
-const applications = JSON.parse(
-localStorage.getItem("applications")
-) || [];
-
-
-
-return(
-
-
-<div className="admin-page">
-
-
-<h1>
-SQ Creator Applications
-</h1>
-
-
-
-<div className="table-box">
-
-
-<table>
-
-
-<thead>
-
-<tr>
-
-<th>
-TikTok
-</th>
-
-<th>
-Instagram
-</th>
-
-<th>
-Reach
-</th>
-
-<th>
-Content
-</th>
-
-<th>
-Other
-</th>
-
-
-</tr>
-
-</thead>
-
-
-
-
-
-<tbody>
-
-
-{
-applications.map((app,index)=>(
-
-
-<tr key={index}>
-
-
-<td>
-@{app.tiktok}
-</td>
-
-
-<td>
-@{app.instagram}
-</td>
-
-
-<td>
-{app.reach}
-</td>
-
-
-<td>
-{app.content}
-</td>
-
-
-<td>
-{app.other || "-"}
-</td>
-
-
-</tr>
-
-
-))
-
-}
-
-
-
-</tbody>
-
-
-</table>
-
-
-</div>
-
-
-
-</div>
-
-
-)
-
-}
-
-
-export default Admin;
+export default function Admin(){
+
+    return(
+      <div style={{
+        minHeight:"100vh",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        flexDirection:"column"
+      }}>
+  
+        <h1>
+          SQ Admin
+        </h1>
+  
+        <p>
+          Dashboard coming soon.
+        </p>
+  
+      </div>
+    )
+  
+  }
